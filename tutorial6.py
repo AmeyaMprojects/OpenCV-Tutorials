bb179f2b052bd2a54a0fa998ca845a6f8217183f
Ameya Mhatre <ameyam.projects@gmail.com>
2024-12-17 19:14:01 +0530

    chin tapak dum dum

--- tutorial6.py
+++ tutorial6.py
@@ -2,15 +2,18 @@ import numpy as np
 import cv2
 
 img = cv2.imread('assets/chessboard.png')
-img = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# img = cv2.resize(img, (0, 0), fx=0.65, fy=0.65)
+if img is None: # check if image was loaded
+    print(f"Error: Could not load image from {img_path}")
+    exit()
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert to grayscale
 
-corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 10)
-corners = np.int0(corners)
+corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 50) # actually detects croners and returns their coordinates
+corners = np.int16(corners) # convert to integers
 
-for corner in corners:
-	x, y = corner.ravel()
-	cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
+for corner in corners: # draw circles on corners loop
+	x, y = corner.ravel() # ravel() flattens the array
+	cv2.circle(img, (x, y), 5, (255, 0, 0), 3) # draw a circle at the corner
 
 for i in range(len(corners)):
 	for j in range(i + 1, len(corners)):
